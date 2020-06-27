445964b7f7d59dd1396b69ea97847b9282986c6f
Technote <technote.space@gmail.com>
2020-06-27 12:40:16 +0900

    Revert "chore: run test"
    This reverts commit 235620d4367096d5820256d3431bdd8933cb9354.

--- src/Collection.php
+++ src/Collection.php
@@ -4,7 +4,6 @@
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space
- * test
  */
 
 namespace Technote;
